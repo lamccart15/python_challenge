5784bbd18e1b5787e66ee42653820e669b3577dc
lamccart15 <lamccart15@gmail.com>
2021-03-16 17:24:35 -0400

    Updated resources and initiated code

---
+++ PyBank/main.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+csvpath = os.path.join()
+#total number of months included in the dataset 
+
+#net total amount of "Profit/Losses" over the entire period
+
+#calculate changes in "Profit/Losses" over the entire period 
+
+    #find the average of the above changes
+
+#Calculate the greatest increase in profits (date and amount) over period
+
+#Calculate the greatest decrease in losses (date and amount) over period
+
+#print analyzis to the terminal 
+
+#export text file with results 
---
+++ PyPoll/main.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+#Calculate total number of votes cast
+
+#Provide complete list of candidates who recieved votes
+
+#Calculate percentage of votes each candidate won
+
+#Calculate total number of votes each candidate won
+
+#Calculate winner of the election based on popular vote 
+
+#print analysis in terminal
+
+#export text file with results 
