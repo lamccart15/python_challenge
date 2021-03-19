932e32a8431690cee67a021522340de31fee66fc
lamccart15 <lamccart15@gmail.com>
2021-03-19 17:21:43 -0400

    Updated code for profit change

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,54 +11,49 @@ with open(csvpath) as csvfile:
 
 # Initiate counter and define starting value 
     month_count = 0
-    total_profit = 0
-    total_loss = 0
     net_total = 0
-    profit = 0
     profit_change = 0 
 
 # Start loop after header
     header = next(csvreader)
+ 
+    profit_list = []
 
 # total number of months included in the dataset
 # total amount of Profit/Loss over entire period
-    for row in csvreader:
-        month_count += 1 
+    for row in csvreader: 
         net_total += int(row[1])
-    #for row in csvreader:
-        #profit = int(row[1])
-    #if profit > 0:
-       # total_profit = total_profit + int(row[1])
-    #elif profit < 0:
-        #total_loss = total_loss + int(row[1])
-
-        #net_total = total_profit - total_loss 
-
-    for x in range(row):
-        profit_change = int(x[1])-int((x-1)[1])
-        #profit_change += profit_change 
-       
-# i from i+1  = change in profit 
-#continue through loop and add each to change in profit 
+        profit_list.append(int(row[1]))
+        month_count += 1
 
 #calculate changes in "Profit/Losses" over the entire period 
+    months_list = []
+    profit_change = []
+    a = 0 
+    b = 1 
+while b < len(profit_list):
+    change = profit_list[b] - profit_list[a]
+    profit_change.append(change)
+    a = b 
+    b = b + 1 
 
-    #find the average of the above changes
+total_profit_change = sum(profit_change)
 
-#Calculate the greatest increase in profits (date and amount) over period
-#create a dictionary grt_profit = {row[0]:int(row[1])}
+#find the average of the above changes
+average_profit_change = round(total_profit_change / len(profit_change), 2)
 
-#Calculate the greatest decrease in losses (date and amount) over period
+#Calculate the greatest increase in profits and greatest decrease in losses (date and amount) over period
+max_profit_increase = max(profit_change)
+max_profit_decrease = min(profit_change)
 
 #print analysis to the terminal 
 print("Financial Analysis")
 print("--------------------------")
 print("Total Months: " + str(month_count))
 print("Total: " + "$" + str(net_total))
-print(profit_change)
-#print("Average change: " + "$" + average_change)
-#print("Greatest Increase in Profits: " + date + grt_profit)
-#print("Greatest Decrease in Profits: " + date + grt_loss)
+print("Average change: " + "$" + str(average_profit_change))
+#print("Greatest Increase in Profits: " + date + max_profit_increase)
+#print("Greatest Decrease in Profits: " + date + max_profit_decrease)
 
 
 #export text file with results 
